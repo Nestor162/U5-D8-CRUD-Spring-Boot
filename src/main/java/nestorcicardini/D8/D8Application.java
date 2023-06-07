279b7f570981a5c73076a9fb093c344e595c5475
Nestor162 <nestorcicardini@gmail.com>
2023-06-07 15:47:22 +0200

    Struttura iniziale progetto

---
+++ src/main/java/nestorcicardini/D8/D8Application.java
@@ -0,0 +1,13 @@
+package nestorcicardini.D8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D8Application.class, args);
+	}
+
+}
---
+++ src/test/java/nestorcicardini/D8/D8ApplicationTests.java
@@ -0,0 +1,13 @@
+package nestorcicardini.D8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
